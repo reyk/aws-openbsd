c5f1ba30ac4db9561065044ab2e677e318a06822
Reyk Floeter <reyk@esdenera.com>
2017-07-07 19:50:42 +0200

    Run syspatch in the installer script before reboot

--- auto_install.sh
+++ auto_install.sh
@@ -7,4 +7,13 @@ for _if in /mnt/etc/hostname.*0; do
 	echo "!/usr/local/libexec/cloud-agent \"\\\$if\"" >>$_if
 done
 
+cat | chroot /mnt <<EOF
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin
+_syspatch=\$(syspatch -c 2>/dev/null)
+if [[ -n "\$_syspatch" ]]; then
+	echo "Running syspatch"
+	syspatch
+fi
+EOF
+
 exit 0
