573b3c15339c1b99a47d6f67f7a492bdcb70c5b0
Reyk Floeter <reyk@esdenera.com>
2018-05-07 19:29:36 +0200

    Add support for 6.3's version of reorder_kernel

--- data/auto_install.sh
+++ data/auto_install.sh
@@ -14,8 +14,20 @@ done
 
 _syspatch=$(syspatch -c 2>/dev/null)
 if [[ -n "$_syspatch" ]]; then
-	# 6.1 installs mp kernels afterwards, so fake it for syspatch
+	# installer copies mp kernels afterwards, so fake it for syspatch
 	cp /bsd /bsd.sp
+
+	# Determine which kernel image is used (NCPU is not exported)
+	NCPU=$(sysctl -n hw.ncpufound)
+	if [ $NCPU -gt 1 ]; then
+		export REORDER_KERNEL=GENERIC.MP
+	else
+		export REORDER_KERNEL=GENERIC
+	fi
+
+	# There is currently no better way to patch reorder_kernel
+	sed -i.dist 's/KERNEL=\${KERNEL%#\*}/KERNEL=${REORDER_KERNEL:-${KERNEL%#*}}/g' \
+		/usr/libexec/reorder_kernel
 
 	echo "Running syspatch"
 	syspatch
